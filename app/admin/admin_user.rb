48d0b77e72911fffe9f3723e4afcd8dd62aced6e
Franky W. (franklinwahl - Frankys-MacBook-Pro.local) <wahl77+git@gmail.com>
2013-07-13 11:54:20 -0700

    Now can logout from admin panel

--- app/admin/admin_user.rb
+++ app/admin/admin_user.rb
@@ -1,17 +1,14 @@
-ActiveAdmin.register AdminUser do     
+ActiveAdmin.register User do     
   index do                            
-    column :email                     
-    column :current_sign_in_at        
-    column :last_sign_in_at           
-    column :sign_in_count             
+    column :username                     
     default_actions                   
   end                                 
 
-  filter :email                       
+  filter :username                       
 
   form do |f|                         
     f.inputs "Admin Details" do       
-      f.input :email                  
+      f.input :username                  
       f.input :password               
       f.input :password_confirmation  
     end                               
